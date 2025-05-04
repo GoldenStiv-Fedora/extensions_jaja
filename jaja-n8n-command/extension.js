a6bd2cd7227ccc3b4bc35129eb4b90248aa15bc0
GoldenStiv-Fedora <goldenstiv@gmail.com>
2025-05-04 18:49:33 +0300

    Update extension.js

--- jaja-n8n-command/extension.js
+++ jaja-n8n-command/extension.js
@@ -5,45 +5,46 @@ import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
 import * as PopupMenu from 'resource:///org/gnome/shell/ui/popupMenu.js';
 
-let indicator;
+let indicator = null;
 
-function init() {
-    // Необязательная инициализация, можно оставить пустой
-}
+const Extension = class {
+    enable() {
+        indicator = new PanelMenu.Button(0.0, 'n8n Command Input', false);
 
-function enable() {
-    indicator = new PanelMenu.Button(0.0, 'n8n Command Input', false);
+        const icon = new St.Icon({
+            gicon: Gio.icon_new_for_string(`${Me.path}/icons/icon-symbolic.svg`),
+            style_class: 'system-status-icon',
+        });
 
-    const icon = new St.Icon({
-        gicon: Gio.icon_new_for_string(`${Me.path}/icons/icon-symbolic.svg`),
-        style_class: 'system-status-icon',
-    });
+        indicator.add_child(icon);
 
-    indicator.add_child(icon);
+        const menuItem = new PopupMenu.PopupMenuItem('Открыть n8n-интерфейс команд');
 
-    const menuItem = new PopupMenu.PopupMenuItem('Открыть n8n-интерфейс команд');
+        menuItem.connect('activate', () => {
+            try {
+                Gio.Subprocess.new(
+                    ['xdg-open', 'http://localhost:5678/webhook/command-entry'],
+                    Gio.SubprocessFlags.NONE
+                );
+            } catch (e) {
+                logError(e, 'Ошибка запуска интерфейса команд');
+            }
+        });
 
-    menuItem.connect('activate', () => {
-        try {
-            Gio.Subprocess.new(
-                ['xdg-open', 'http://localhost:5678/webhook/command-entry'],
-                Gio.SubprocessFlags.NONE
-            );
-        } catch (e) {
-            log(`Ошибка запуска интерфейса команд: ${e}`);
-        }
-    });
-
-    indicator.menu.addMenuItem(menuItem);
-
-    Main.panel.addToStatusArea('n8n-command-indicator', indicator);
-}
-
-function disable() {
-    if (indicator) {
-        indicator.destroy();
-        indicator = null;
+        indicator.menu.addMenuItem(menuItem);
+        Main.panel.addToStatusArea('n8n-command-indicator', indicator);
     }
-}
 
-export { init, enable, disable };
+    disable() {
+        if (indicator) {
+            indicator.destroy();
+            indicator = null;
+        }
+    }
+
+    init() {
+        // можно оставить пустым
+    }
+};
+
+export default Extension;
