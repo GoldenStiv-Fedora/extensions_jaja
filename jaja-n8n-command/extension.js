1f1adccc91b90b7509c502888845a834e696b221
GoldenStiv-Fedora <goldenstiv@gmail.com>
2025-05-04 16:15:22 +0300

    Update extension.js

--- jaja-n8n-command/extension.js
+++ jaja-n8n-command/extension.js
@@ -1,54 +1,79 @@
-import St from 'gi://St';
-import Main from 'resource:///org/gnome/shell/ui/main.js';
-import PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
-import { Gio, GLib, Soup } from 'gi://Gio';
-import PopupMenu from 'resource:///org/gnome/shell/ui/popupMenu.js';
+// extension.js
 
-let JaJaExtension;
+import { St, Gio, GLib } from 'resource:///org/gnome/shell/ui/main.js';
+import * as Main from 'resource:///org/gnome/shell/ui/main.js';
+import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
+import * as PopupMenu from 'resource:///org/gnome/shell/ui/popupMenu.js';
+import * as MessageTray from 'resource:///org/gnome/shell/ui/messageTray.js';
 
-class JajaCommandExtension extends PanelMenu.Button {
-  constructor() {
-    super(0.0, 'JAJA N8N Command', false);
+let jajaIndicator;
 
-    const icon = new St.Icon({
-      gicon: Gio.icon_new_for_string(Me.path + '/icon.svg'),
-      style_class: 'system-status-icon',
-    });
-    this.add_child(icon);
+class JajaCommandIndicator extends PanelMenu.Button {
+    constructor() {
+        super(0.0, 'JAJA Command Button', false);
 
-    // Поле ввода команды
-    const box = new St.BoxLayout({ vertical: false });
-    this.entry = new St.Entry({ style_class: 'jaja-entry', can_focus: true });
-    box.add_child(this.entry);
+        const icon = new St.Icon({
+            gicon: Gio.icon_new_for_string(Me.dir.get_path() + '/icon.svg'),
+            style_class: 'system-status-icon',
+        });
 
-    // Кнопка отправки
-    const button = new St.Button({ label: 'Отправить', style_class: 'jaja-button' });
-    button.connect('clicked', () => this._sendCommand());
-    box.add_child(button);
+        this.add_child(icon);
 
-    const item = new PopupMenu.PopupBaseMenuItem({ reactive: false });
-    item.actor.add_child(box);
-    this.menu.addMenuItem(item);
-  }
+        const entryItem = new PopupMenu.PopupBaseMenuItem({ reactive: false });
+        this.entry = new St.Entry({
+            hint_text: 'Введите команду для JAJA',
+            track_hover: true,
+            can_focus: true,
+        });
 
-  _sendCommand() {
-    const session = new Soup.Session();
-    const message = Soup.form_request_new_from_hash('POST', 'http://localhost:5678/webhook/command-input', {
-      command: this.entry.get_text(),
-    });
-    session.send_and_read_async(message, 0, null, null);
-    Main.notify('JAJA Agent', `Команда отправлена: ${this.entry.get_text()}`);
-    this.entry.set_text('');
-  }
+        entryItem.add_child(this.entry);
+        this.menu.addMenuItem(entryItem);
+
+        const sendItem = new PopupMenu.PopupMenuItem('Отправить');
+        sendItem.connect('activate', () => {
+            let command = this.entry.get_text();
+            this.sendCommand(command);
+        });
+
+        this.menu.addMenuItem(sendItem);
+    }
+
+    sendCommand(command) {
+        try {
+            let subprocess = Gio.Subprocess.new(
+                ['curl', '-X', 'POST', '-H', 'Content-Type: application/json',
+                 '-d', JSON.stringify({ text: command }),
+                 'http://localhost:5678/webhook/command-input'],
+                Gio.SubprocessFlags.NONE
+            );
+
+            subprocess.init(null);
+            this._showNotification('Команда отправлена', command);
+        } catch (e) {
+            this._showNotification('Ошибка', e.message);
+        }
+    }
+
+    _showNotification(title, text) {
+        const source = new MessageTray.SystemNotificationSource();
+        Main.messageTray.add(source);
+
+        const notification = new MessageTray.Notification(source, title, text);
+        notification.setTransient(true);
+        source.notify(notification);
+    }
 }
 
 function init() {}
 
 function enable() {
-  JaJaExtension = new JajaCommandExtension();
-  Main.panel.addToStatusArea('jaja-n8n-command', JaJaExtension);
+    jajaIndicator = new JajaCommandIndicator();
+    Main.panel.addToStatusArea('jaja-n8n-command', jajaIndicator);
 }
 
 function disable() {
-  JaJaExtension.destroy();
+    if (jajaIndicator) {
+        jajaIndicator.destroy();
+        jajaIndicator = null;
+    }
 }
